5ff387d6d20f6b04425f7116902326b2cfd8f105
hiteshcemba <hitesh.cemba@gmail.com>
2015-12-01 16:40:19 -0500

    1 commit

---
+++ SampleWebAPI/Controllers/SampleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SampleWebAPI.Controllers
+{
+    public class SampleController : ApiController
+    {
+        public string GetTime([FromUri]Person t)
+        {
+            return string.Format("Received Name: {0}", t.Name);
+        }
+    }
+
+    public class Person
+    {
+        public String Name { get; set; }
+      
+    }
+}
---
+++ SampleWebAPI/Scripts/myapp.js
@@ -0,0 +1,95 @@
+﻿
+$(document).ready(function () {
+
+    $('#loading').hide();
+});
+
+$(document).ajaxStart(function () {
+    $('#loading').show();
+}).ajaxStop(function () {
+    $('#loading').hide();
+});
+
+
+function GetAllProducts()
+
+{
+    jQuery.support.cors = true;
+    //alert('GetAllProducts');
+    $.ajax({
+        url: 'http://localhost:35024/api/Products/',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            WriteResponse(data);
+        },
+        error: function (x, y, z) {
+            alert(x + '\n' + y + '\n' + z);
+        }
+    });
+
+ 
+
+}
+
+
+function WriteResponse(products) {
+
+    var strResult = "<table><th>ID</th><th>Name</th><th>Category</th><th>Price</th>";
+    $.each(products, function (index, product) {
+        strResult += "<tr><td>" + product.Id + "</td><td> " + product.Name + "</td><td>" + product.Category + "</td><td>" + product.Price + "</td></tr>";
+    });
+    strResult += "</table>";
+    $("#divResult").html(strResult);
+}
+
+function WriteResponse2(product) {
+
+    var strResult = "<table><th>ID</th><th>Name</th><th>Category</th><th>Price</th>";
+     strResult += "<tr><td>" + product.Id + "</td><td> " + product.Name + "</td><td>" + product.Category + "</td><td>" + product.Price + "</td></tr>";
+      strResult += "</table>";
+    $("#divResult").html(strResult);
+}
+
+
+function GetProduct() {
+    jQuery.support.cors = true;
+    var id = $('#txtProdId').val();
+    $.ajax({
+        url: 'http://localhost:35024/api/Products/' + id,
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            WriteResponse2(data);
+        },
+        error: function (x, y, z) {
+            alert(x + '\n' + y + '\n' + z);
+        }
+    });
+}
+
+
+function AddProduct() {
+    jQuery.support.cors = true;
+    var product = {
+        Id: $('#txtProdId2').val(),
+        Name: $('#txtName').val(),
+        Category: $('#txtCategory').val(),
+        Price: $('#txtPrice').val()
+    };
+
+    $.ajax({
+        url: 'http://localhost:35024/api/Products/',
+        type: 'POST',
+        data: JSON.stringify(product),
+        contentType: "application/json;charset=utf-8",
+        success: function (data) {
+            WriteResponse(data);
+        },
+        error: function (x, y, z) {
+            alert(x + '\n' + y + '\n' + z);
+        }
+    });
+}
+
+
